4bff309da43ec467fd69de498d2cf0919a2288b2
Roman <anufriev.roman@protonmail.com>
2023-08-29 15:43:44 +0900

    Improving translation

--- waypaper/translation_en.py
+++ waypaper/translation_en.py
@@ -17,14 +17,16 @@ MSG_CHANGEFOLDER = "Change wallpaper folder"
 MSG_CACHING = "Caching wallpapers..."
 MSG_SETWITH = "Wallpaper was set with"
 
-MSG_HELP = "Waypaper's hotkeys:\n\nhjkl - Navigation (←↓↑→)\ng - Scroll to top\n"
-MSG_HELP += "G - Scroll to bottom\nr - Recache images"
-MSG_HELP += "\ns - Include/exclude images in subfolders\n? - Help\nq - Exit\n\n"
+MSG_HELP = "Waypaper's hotkeys:\n\nhjkl - Navigation (←↓↑→)\n"
+MSG_HELP += "g - Scroll to top\nG - Scroll to bottom\n"
+MSG_HELP += "s - Include/exclude images in subfolders\n? - Help\nq - Exit\n\n"
 MSG_HELP += MSG_INFO
 
 ERR_CACHE = "Error deleting cache"
 ERR_BACKEND = "Looks like none of the wallpaper backends is installed in the system.\n"
-ERR_BACKEND += "Use your package manager to install at least one of these backends:\n"
-ERR_BACKEND += "\n- swaybg (for Wayland)\n- swww (for Wayland)\n- feh (for Xorg)\n- wallutils (for Xorg & Wayland)"
+ERR_BACKEND += "Use your package manager to install at least one of these backends:\n\n"
+ERR_BACKEND += "- swaybg (for Wayland)\n- swww (for Wayland)\n"
+ERR_BACKEND += "- feh (for Xorg)\n- wallutils (for Xorg & Wayland)\n\n"
+ERR_BACKEND += MSG_INFO
 ERR_WALL = "Error changing wallpaper: "
 ERR_NOTSUP = "The backend is not supported:"
