e0a432daf3a3389c9b397228afedcfc40b971209
nikolaizombie1 <fabio.matos999@gmail.com>
2024-05-18 20:48:39 +0000

    Add hyprpaper backend support

--- waypaper/options.py
+++ waypaper/options.py
@@ -1,4 +1,4 @@
-BACKEND_OPTIONS = ["none", "swaybg", "swww", "feh", "wallutils"]
+BACKEND_OPTIONS = ["none", "swaybg", "swww", "feh", "wallutils", "hyprpaper"]
 FILL_OPTIONS = ["fill", "stretch", "fit", "center", "tile"]
 SORT_OPTIONS = ["name", "namerev", "date", "daterev"]
 SORT_DISPLAYS = {
@@ -12,6 +12,7 @@ IMAGE_EXTENSIONS = {
         BACKEND_OPTIONS[1]: ['.gif', '.jpg', '.jpeg', '.png', '.webp', '.bmp', '.pnm', '.tiff'],
         BACKEND_OPTIONS[2]: ['.gif', '.jpg', '.jpeg', '.png', '.bmp', '.pnm', '.tiff'],
         BACKEND_OPTIONS[3]: ['.gif', '.jpg', '.jpeg', '.png'],
+        BACKEND_OPTIONS[5]: ['.jpg', '.jpeg', '.png', '.webp'],
         }
 
 SWWW_TRANSITION_TYPES = ["any", "none", "simple", "fade", "wipe",  "left", "right", "top",
