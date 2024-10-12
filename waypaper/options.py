1b282788f712cc12fd8597f742a5cc5c53be5867
Kritagya Bhattarai (CalfMoon) <kritagyabhattarai@proton.me>
2024-10-12 18:11:15 +0545

    feat: add mpvpaper as backend option

--- waypaper/options.py
+++ waypaper/options.py
@@ -1,5 +1,5 @@
 from typing import List, Dict
-BACKEND_OPTIONS: List[str] = ["none", "swaybg", "swww", "feh", "wallutils", "hyprpaper"]
+BACKEND_OPTIONS: List[str] = ["none", "swaybg", "swww", "feh", "wallutils", "hyprpaper", "mpvpaper"]
 FILL_OPTIONS: List[str] = ["fill", "stretch", "fit", "center", "tile"]
 SORT_OPTIONS: List[str] = ["name", "namerev", "date", "daterev"]
 SORT_DISPLAYS: Dict[str, str] = {
@@ -20,6 +20,7 @@ IMAGE_EXTENSIONS: Dict[BACKEND_OPTIONS, List[str]] = {
         BACKEND_OPTIONS[3]: ['.gif', '.jpg', '.jpeg', '.png', '.bmp', '.pnm', '.tiff'],
         BACKEND_OPTIONS[4]: ['.gif', '.jpg', '.jpeg', '.png'],
         BACKEND_OPTIONS[5]: ['.jpg', '.jpeg', '.png', '.webp'],
+        BACKEND_OPTIONS[6]: ['.gif', '.jpg', '.jpeg', '.png', '.webp', '.bmp', '.pnm', '.tiff'] + VIDEO_EXTENSIONS,
         }
 
 SWWW_TRANSITION_TYPES: List[str] = ["any", "none", "simple", "fade", "wipe",  "left", "right", "top",
